f52553bc67e81349ea485cf5d7659086ee125fd2
Agnes Ching <agnesching19@gmail.com>
2018-06-03 13:26:06 +0100

    event acknowledgements

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,10 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
 });
+
+socket.emit('createMessage', {
+  from: 'Bob',
+  text: 'Bello!'
+}, function(data) {
+  console.log('Got it', data);
+});
