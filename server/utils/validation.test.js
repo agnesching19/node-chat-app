9a3b874b3f9b73811e4068e2c2609d635c91e134
Agnes Ching <agnesching19@gmail.com>
2018-06-24 20:46:56 +0100

    Passing room data

---
+++ server/utils/validation.test.js
@@ -0,0 +1,19 @@
+const expect = require('expect');
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non-string value', () => {
+    const res = isRealString(2018);
+    expect(res).toBe(false);
+  });
+
+  it('should reject string with only spaces', () => {
+    const res = isRealString('       ');
+    expect(res).toBe(false);
+  });
+
+  it('should allow string with non-space characters', () => {
+    const res = isRealString('  Hello Simon  ');
+    expect(res).toBe(true);
+  });
+});
